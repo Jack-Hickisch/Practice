1f3299c3376ac055a55a8f5b852ebef29aca648c
Jack Hickisch <jackhickisch@gmail.com>
2019-09-03 12:52:39 -0600

    Time Until setup

--- TimeUntil.java
+++ TimeUntil.java
@@ -1,17 +1,9 @@
 import java.util.Scanner;
 
-public class LogarithmBaseTwo
+public class TimeUntil
 {
     public static void main(String[] args)
     {
         Scanner scan = new Scanner( System.in );
-        Double number_entered, base_two_log;
-
-        System.out.println("Enter your number: ");
-        number_entered = scan.nextDouble();
-        
-        base_two_log = Math.log( number_entered ) / Math.log( 2 );
-
-        System.out.println("The base two log of the number " + number_entered + "is " + base_two_log);
     }
 }
