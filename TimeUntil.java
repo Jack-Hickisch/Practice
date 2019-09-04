21d052535bd5e496691520d1e98b0d26e02af7e0
Jack-Hickisch <jackhickisch@gmail.com>
2019-09-04 16:22:40 -0600

    TimeUntil Working

--- TimeUntil.java
+++ TimeUntil.java
@@ -7,8 +7,8 @@ public class TimeUntil
     {
         Scanner scan = new Scanner( System.in );
         int[] current_time, proposed_time;
-        int proposed_second, proposed_minute, proposed_hour, difference_hour, difference_minute, difference_second, final_hours = 0, final_minutes = 0, final_seconds = 0;
-        String user_entered_time;
+        int proposed_second, proposed_minute, proposed_hour, difference_hour, difference_minute, difference_second;
+        String user_entered_time, final_hours = "", final_minutes = "", final_seconds = "";
         boolean show_hour = true, show_minute = true, show_second = true;
 
         System.out.println("Please enter the time in the format hh:mm:ss");
@@ -25,69 +25,60 @@ public class TimeUntil
         difference_minute = proposed_time[1] - current_time[1];
         difference_second = proposed_time[2] - current_time[2];
 
-        // System.out.println(current_time[0]);
-        // System.out.println(current_time[1]);
-        // System.out.println(current_time[2]);
-
-        // System.out.println("");
-
-        // System.out.println(difference_hour);
-        // System.out.println(difference_minute);
-        // System.out.println(difference_second);
-
-        if (difference_second < 0)
+        if (!(proposed_hour == LocalDateTime.now().getHour() && proposed_minute == LocalDateTime.now().getMinute() && proposed_second == LocalDateTime.now().getSecond()))
         {
-            difference_minute -= 1;
-            difference_second = 60 - ( -1 * difference_second );
+            if (difference_second < 0)
+            {
+                difference_minute -= 1;
+                difference_second = 60 - ( -1 * difference_second );
+            }
+    
+            if (difference_minute < 0)
+            {
+                difference_hour -= 1;
+                difference_minute = 60 - ( -1 * difference_minute );
+            }
+    
+            if (difference_hour < 0)
+            {
+                difference_hour = 24 - ( -1 * difference_hour );
+            }
+    
+            if (difference_hour == 0)
+            {
+                show_hour = false;
+            }
+    
+            if (difference_minute == 0)
+            {
+                show_minute = false;
+            }
+    
+            if (difference_second == 0)
+            {
+                show_second = false;
+            }
+    
+            if (show_hour)
+            {
+                final_hours = Integer.toString(difference_hour) + " hours ";
+            }
+    
+            if (show_minute)
+            {
+                final_minutes = Integer.toString(difference_minute) + " minutes ";
+            }
+    
+            if (show_second)
+            {
+                final_seconds = Integer.toString(difference_second) + " seconds ";
+            }
+    
+            System.out.println("There are " + final_hours + final_minutes + final_seconds + "until then");
         }
-
-        if (difference_minute < 0)
+        else
         {
-            difference_hour -= 1;
-            difference_minute = 60 - ( -1 * difference_minute );
+            System.out.println("That is the current time!");
         }
-
-        if (difference_hour < 0)
-        {
-            difference_hour = 24 - ( -1 * difference_hour );
-        }
-
-        // System.out.println(difference_hour);
-        // System.out.println(difference_minute);
-        // System.out.println(difference_second);
-
-        if (difference_hour == 0)
-        {
-            show_hour = false;
-        }
-
-        if (difference_minute == 0)
-        {
-            show_minute = false;
-        }
-
-        if (difference_second == 0)
-        {
-            show_second = false;
-        }
-
-
-
-        if (show_hour)
-        {
-            final_hours = difference_hour;
-        }
-
-        if (show_minute)
-        {
-            final_minutes = difference_minute;
-        }
-
-        if (show_second)
-        {
-            final_seconds = difference_second;
-        }
-
-        System.out.println("There are " + final_hours + " hours " + final_minutes + " minutes and " + final_seconds + " seconds until then");
     }
 }
